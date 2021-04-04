e43e3de4e36f4cfd27be3eaa6cb70d7856744aa2
Muhamad Rafi Pamungkas <muhamadrafipamungkas@gmail.com>
2021-04-05 06:41:18 +0700

    Sign In with Google Function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
+import { useState } from 'react'
 import './App.css';
 import Chat from './components/Chat';
 import Sidebar from './components/Sidebar';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from './components/Login';
+
 function App() {
+  const [user, setUser] = useState('');
   return (
     <div className="app">
-      <div className="app__body">
-      <Router>
-      <Sidebar />
-        <Switch>
-          <Route path="/rooms/:roomId">
-            <Chat />
-          </Route>
-          <Route path="/">
-            <Chat />
-          </Route>
-        </Switch>
-      </Router>
-      </div>
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+        <Router>
+        <Sidebar />
+          <Switch>
+            <Route path="/rooms/:roomId">
+              <Chat />
+            </Route>
+            <Route path="/">
+              <Chat />
+            </Route>
+          </Switch>
+        </Router>
+        </div>
+      )}
+
     </div>
   );
 }
---
+++ src/components/Login/index.js
@@ -0,0 +1,30 @@
+import { Button } from '@material-ui/core'
+import React from 'react'
+import { auth, provider } from '../../config/firebase'
+import './Login.css'
+
+function Login() {
+    const signIn = () => {
+        auth.signInWithPopup(provider)
+        .then(result => console.log(result))
+        .catch(err => console.log(err))
+    }
+    return (
+        <div className="login">
+            <div className="login__container">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="Whatsapp Logo"/>
+                <div className="login__text">
+                    <h1>
+                        Sign in to Whatsapp Clone
+                    </h1>
+                </div> 
+
+                <Button type="submit" onClick={signIn}>
+                    Sign In With Google
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default Login
